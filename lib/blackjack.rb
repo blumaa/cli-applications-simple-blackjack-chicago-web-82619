f939b73e2561e9b52fa8264cebb515d9ea2bc09b
Aaron Blum <blumaa@gmail.com>
2019-08-06 16:13:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,7 @@ end
 
 def hit?(card_total)
   prompt_user
-  # letter = get_user_input
+  letter = get_user_input
   # binding.pry
   # if letter == 's'
   #   puts "Your card total is #{card_total}."
